48dc0b0a2c5c72a4ae48c6b0d9629a33da789629
Ozair Haghdoust <Ozair-Haghdoust@outlook.com>
2022-07-15 17:26:19 +0100

    All list views working

--- src/main/java/com/example/shopping_system/TakeOrders.java
+++ src/main/java/com/example/shopping_system/TakeOrders.java
@@ -1,20 +1,26 @@
 package com.example.shopping_system;
 
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class TakeOrders implements Initializable {
 
+   private String mylist;
     @FXML
     private ListView<String> BurgerList;
 
@@ -27,11 +33,11 @@ public class TakeOrders implements Initializable {
     @FXML
     private ListView<String> DrinksList;
 
+@FXML
+private ListView<String> OrderList;
 
 
 
-    @FXML
-    private TextArea OrderList;
 
     @FXML
     private Button display;
@@ -71,6 +77,17 @@ validateLogin("burgers", BurgerList);
         validateLogin("pizza", PizzaList);
 
 
+        DrinksList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+                mylist = DrinksList.getSelectionModel().getSelectedItem();
+
+
+                OrderList.getItems().add(mylist);
+
+            }
+        });
 
     }
+
 }
