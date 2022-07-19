048c3287f0cc4072daed419baaa190e028e7a44d
Ozair Haghdoust <Ozair-Haghdoust@outlook.com>
2022-07-19 17:06:31 +0100

    all calculations working

--- src/main/java/com/example/shopping_system/TakeOrders.java
+++ src/main/java/com/example/shopping_system/TakeOrders.java
@@ -10,22 +10,34 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
+import org.controlsfx.control.PropertySheet;
 
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.text.DecimalFormat;
 import java.util.*;
 
 public class TakeOrders implements Initializable {
 
-private  String mylist;
+
+   private   Float total = 0.0f, change = 0.0f  ,tax = 0.8f   , cash = 0.0f   ;
+
+private  String mylist,  chosen_payment = " " ;
+
+public ArrayList<Float> arrayList = new ArrayList<>();
+
+@FXML
+private TextField cashtxt,  changetxt, taxtxt, subtotaltxt, totaltxt ;
+
+
+
+@FXML
+private ChoiceBox<String > payment_method;
     @FXML
     private TableView<burger> burger_table;
     @FXML
@@ -34,7 +46,15 @@ private  String mylist;
     private TableView<com.example.shopping_system.TakeOrder_classes.meal_deals> meal_deals;
 
     @FXML
+    private TableView checkout;
+    @FXML
     private TableView<pizza> pizza_table;
+
+    @FXML
+    private TableColumn <Products, String> checkout_order;
+    @FXML
+    private TableColumn<Products, Float> checkout_price;
+
 
     // burger table columns
     @FXML
@@ -68,10 +88,11 @@ private  String mylist;
     private ObservableList<burger> observableListburger = FXCollections.observableArrayList();
     private ObservableList<meal_deals> observableListmeal_deals = FXCollections.observableArrayList();
     private ObservableList<drinks> observableListdrinks = FXCollections.observableArrayList();
+    private ObservableList<Products> observableList = FXCollections.observableArrayList();
 
 
     @FXML
-    private ListView<String> OrderList;
+    private ListView<Products> OrderList;
 
 
     @FXML
@@ -91,8 +112,8 @@ private  String mylist;
         mealdeals_price.setCellValueFactory(new PropertyValueFactory<meal_deals, Float>("mealdeal_price"));
         drinks_order.setCellValueFactory(new PropertyValueFactory<drinks, String>("drinks_order"));
         drinks_price.setCellValueFactory(new PropertyValueFactory<drinks, Float>("drinks_price"));
-
-
+        checkout_order.setCellValueFactory(new PropertyValueFactory<Products, String>("Order"));
+        checkout_price.setCellValueFactory(new PropertyValueFactory<Products, Float>("Price"));
         String product = "select * from shopping_project.product_catalogue where Product_Type =  '" + "burgers" + "'";
 
         try {
@@ -164,66 +185,159 @@ private  String mylist;
 
         ProductRecords();
 
+        //payment method
+        String[] paymentmethod = {"Card","Cash"};
+payment_method.getItems().addAll(paymentmethod);
 
         pizza_table.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
 
-                mylist = String.valueOf(pizza_table.getSelectionModel().getSelectedItem());
-                OrderList.getItems().add(mylist);
+                pizza pizza = new pizza();
+
+                pizza = pizza_table.getItems().get(pizza_table.getSelectionModel().getSelectedIndex());
+                String order = pizza.getPizza_order();
+                Products products = new Products();
+                Float price = pizza.getPizza_price();
+                products.setOrder(order);
+                products.setPrice(price);
+
+                observableList.add(new Products(order, price));
+                checkout.setItems(observableList);
+
+                arrayList.add(products.getPrice());
+
             }
         });
-
 
         burger_table.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
 
-                mylist = String.valueOf(burger_table.getSelectionModel().getSelectedItem());
-                OrderList.getItems().add(mylist);
+                burger burger = new burger();
 
-                // after button is clicked the item should be added to the table view
+                burger = burger_table.getItems().get(burger_table.getSelectionModel().getSelectedIndex());
+                String order = burger.getBurger_order();
+                Products products = new Products();
+                Float price = burger.getBurger_price();
+                products.setOrder(order);
+                products.setPrice(price);
 
+                observableList.add(new Products(order, price));
+                checkout.setItems(observableList);
 
+                arrayList.add(products.getPrice());
 
             }
         });
-        drinks_table.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
 
-                mylist = String.valueOf(drinks_table.getSelectionModel().getSelectedItem());
-                OrderList.getItems().add(mylist);
-            }
-        });
+
         meal_deals.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
 
-                mylist = String.valueOf(meal_deals.getSelectionModel().getSelectedItem());
-                OrderList.getItems().add(mylist);
+                meal_deals meal_deals1 = new meal_deals();
+
+                meal_deals1 = meal_deals.getItems().get(meal_deals.getSelectionModel().getSelectedIndex());
+                String order = meal_deals1.getMealdeal_order();
+                Products products = new Products();
+                Float price = meal_deals1.getMealdeal_price();
+                products.setOrder(order);
+                products.setPrice(price);
+
+                observableList.add(new Products(order, price));
+                checkout.setItems(observableList);
+
+                arrayList.add(products.getPrice());
+
             }
         });
 
-        OrderList.setOnMouseClicked(new EventHandler<MouseEvent>() {
+        drinks_table.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                OrderList.getItems().remove(OrderList.getSelectionModel().getSelectedIndex());
+
+                drinks drinks = new drinks();
+
+                drinks = drinks_table.getItems().get(drinks_table.getSelectionModel().getSelectedIndex());
+                String order = drinks.getDrinks_order();
+                Products products = new Products();
+                Float price = drinks.getDrinks_price();
+                products.setOrder(order);
+                products.setPrice(price);
+
+                observableList.add(new Products(order, price));
+                checkout.setItems(observableList);
+
+                arrayList.add(products.getPrice());
+
+            }
+        });
+        checkout.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                checkout.getItems().remove(checkout.getSelectionModel().getSelectedIndex());
 
             }
         });
     }
 
+    public void total(ActionEvent event) {
+ total = 0.0f;
+ change = 0.0f;
+ tax = 0.8f;
+ cash = 0.0f;
 
-    public void calculate(ActionEvent event) {
-        String[] split;
-        for (int i = 0; i < OrderList.getItems().toArray().length; i++) {
-            System.out.println(OrderList.getItems().get(i));
+ DecimalFormat decimalFormat = new DecimalFormat("#.##");
 
+
+
+ chosen_payment = payment_method.getValue();
+
+
+
+
+        for(int i = 0; i < arrayList.toArray().length; i++){
+            total+=arrayList.get(i);
         }
+        Float format = Float.valueOf(decimalFormat.format(tax));
+        format = Float.valueOf(decimalFormat.format(total));
+
+        subtotaltxt.setText(String.valueOf(total));
+
+
+        taxtxt.setText(String.valueOf(tax));
+
+
+
+        Float postTex = total * tax;
+        format = Float.valueOf(decimalFormat.format(postTex));
+
+        tax = total - format;
+        total = tax + total;
+         format = Float.valueOf(decimalFormat.format(total));
+total =format;
+        totaltxt.setText(String.valueOf( format ));
 
 
     }
 
 
-}
+    public void change(ActionEvent event){
+
+        if( cashtxt.getText().isBlank() && chosen_payment.equals("Cash")){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setHeaderText("cash required");
+            alert.showAndWait();
+        }else{
+            cash = Float.valueOf(cashtxt.getText());
+            change = cash - total;
+            DecimalFormat decimalFormat = new DecimalFormat("#.##");
+            Float format = Float.valueOf(decimalFormat.format(change));
+
+            changetxt.setText(String.valueOf(format));
+        }
+
+    }
+    }
+
